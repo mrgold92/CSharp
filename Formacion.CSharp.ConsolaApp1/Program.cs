24b4bcf6275f81e5d8e5b29ad83fb3fc60fb1e45
David Salazar <davids1_4@hotmail.com>
2021-05-13 10:39:37 +0200

    Agregar archivos de proyecto.

---
+++ Formacion.CSharp.ConsolaApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Formacion.CSharp.ConsolaApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+}
