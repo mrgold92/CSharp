4a5a48af6c0a2baf58c70cc638af8eb1dc3854d3
David Salazar <davids1_4@hotmail.com>
2021-05-18 21:20:28 +0200

    Añadido ejercicio G

---
+++ AA-Ejercicios/G/G.cs
@@ -0,0 +1,28 @@
+﻿using System;
+/*******************************************************************
+* G1. Pregunta al operador cualquier cosa.
+* G2. Muestra por Simón dice xxxxxxxxx incluyendo la respuesta
+* del operador
+* G3. Esta secuencia de pregunta/mostrar en pantalla se repite hasta
+* que el operado responde fin
+********************************************************************/
+namespace Ejercicios
+{
+    class G
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("¿De qué color es el caballo blanco de Santiago? ");
+                string res = Console.ReadLine();
+                
+                if (res.ToLower().Trim() == "fin") break;
+
+                Console.WriteLine($"Simón dice {res}.");
+            }
+
+
+        }
+    }
+}
