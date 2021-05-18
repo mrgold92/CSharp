c93a7a7a0164dbdfa8ac9f30cea0e31d565b8de5
David Salazar <davids1_4@hotmail.com>
2021-05-18 14:29:06 +0200

    Clase 18/05/2021

--- Formacion.CSharp.ConsolaApp3/Metodos.cs
+++ Formacion.CSharp.ConsolaApp3/Metodos.cs
@@ -7,6 +7,21 @@ namespace Formacion.CSharp.ConsolaApp3
     {
         static void Main(string[] args)
         {
+
+
+            /*==========================================
+             *
+             * Delegados y Eventos
+             *
+             *==========================================*/
+
+
+            /*==========================================
+             *
+             * Objetos, constructores, getters y settes
+             *
+             *==========================================*/
+
             Alumno alumno = new Alumno() { Nombre = "Pepe", Apellidos = "Sánchez", Edad = 29 };
             //Retornamos un console.writeline
             alumno.PintarNombre(100, "Sr.");
@@ -18,10 +33,18 @@ namespace Formacion.CSharp.ConsolaApp3
             alumno.PintarNombre(0);
             alumno.PintarNombre(100, param2: "DEMO");
 
+            Console.WriteLine($"Retornando propio objeto: {alumno.GetObject()}");
+            Console.WriteLine("Alumno nombre completo: {0}", alumno.nombreCompleto);
+
 
             //Retornamos un string
             Console.WriteLine(alumno.NombreCompleto());
 
+            /*==========================================
+            *
+            * Paso por referencia y por valor
+            *
+            *==========================================*/
 
             Calculos calculos = new Calculos();
             int nm1 = 25;
@@ -33,6 +56,35 @@ namespace Formacion.CSharp.ConsolaApp3
             Console.WriteLine("Num1: {0} - Num2: {1} - Resultado: {2}", nm1, nm2, result);
             calculos.suma(nm1, nm2, ref result);
             Console.WriteLine("Num1: {0} - Num2: {1} - Resultado: {2}", nm1, nm2, result);
+
+            Console.WriteLine("------------------------");
+
+
+            /*==========================================
+            *
+            * Class y Struct
+            *
+            *==========================================*/
+
+            //clase
+            Alumno alumno1 = new Alumno() { Nombre = "María José", Apellidos = "Sanz" };
+            Console.WriteLine("Alumno: {0} {1}", alumno1.Nombre, alumno1.Apellidos);
+            calculos.TomarDatos(alumno1);
+            Console.WriteLine("Alumno: {0} {1}", alumno1.Nombre, alumno1.Apellidos);
+
+            Console.WriteLine("------------------------");
+
+            //struct
+            //Struct es de tipo valor, no referencia, como las clases, por lo tanto no cambian el objeto original
+            //Struct está pensando para pequeños objetos, si son muy grandes, mejor una class
+            Alumno2 alumno2 = new Alumno2() { Nombre = "María José", Apellidos = "Sanz" };
+            Console.WriteLine("Alumno: {0} {1}", alumno2.Nombre, alumno2.Apellidos);
+            calculos.TomarDatos2(alumno2);
+            Console.WriteLine("Alumno: {0} {1}", alumno2.Nombre, alumno2.Apellidos);
+
+            //Si queremos pasarlo como referencia, en un struct, deberemos poner ref.
+            calculos.TomarDatos2Ref(ref alumno2);
+            Console.WriteLine("Alumno: {0} {1}", alumno2.Nombre, alumno2.Apellidos);
         }
     }
 }
@@ -42,41 +94,131 @@ namespace Formacion.CSharp.Objects
 {
     public class Alumno
     {
-        public string Nombre = "Pepe";
-        public string Apellidos = "García";
-        public int Edad = 28;
+        private string nombre;
+        private string apellidos;
+        private int edad;
+
+        // Sobrecarga constructores
+        public Alumno()
+        {
+
+        }
+
+        public Alumno(string nombre)
+        {
+            this.nombre = nombre;
+        }
+
+        public Alumno(string nombre, string apellidos)
+        {
+            this.nombre = nombre;
+            this.Apellidos = apellidos;
+        }
+
+        public Alumno(string nombre, string apellidos, int edad)
+        {
+            this.nombre = nombre;
+            this.Apellidos = apellidos;
+            this.Edad = edad;
+        }
+
+
+        //Getters y Setters
+
+        // Forma 1
+        public int Edad
+        {
+            get
+            {
+                return edad;
+            }
+
+            set
+            {
+                if (value >= 18)
+                {
+                    edad = value;
+                }
+            }
+        }
+
+        public string nombreCompleto
+        {
+            get
+            {
+                return $"{this.Nombre} {this.Apellidos}";
+            }
+        }
+
+
+        //Forma 2
+        public string Nombre { get => nombre; set => nombre = value; }
+        //public int Edad { get => edad; set => edad = value; }
+
+        //Forma 3
+        //No necesitaríamos en este caso la variable de arriba
+        public string Apellidos { get; set; }
+
+
+
+
+
+        // Destructores: no tienen tipo, comienza por ~ más el nombre de la clase
+        // No se le puede llamar y se ejecuta automáticamente.
+        // No recibe parámetros.
+        ~Alumno()
+        {
+            Console.WriteLine("Inicio destructor de Alumno.");
+
+            this.nombre = null;
+            this.Apellidos = null;
+        }
+
 
         /// <summary>
         /// Pinta el nombre y los apellidos del Alumno
         /// </summary>
         /// <param name="modo"></param>
         /// <param name="prefijo"></param>
+        /// Los parámetros opcionales deben ir siempre detrás de todos los parámetros obligatorios.
         public void PintarNombre(int modo, string prefijo = "Don", string param1 = "", string param2 = "")
         {
             if (prefijo.Length > 0)
             {
                 if (modo == 100)
                 {
-                    Console.WriteLine($"{prefijo} {this.Apellidos}, {this.Nombre} ");
+                    Console.WriteLine($"{prefijo} {this.Apellidos}, {this.nombre} ");
                 }
                 else
                 {
-                    Console.WriteLine($"{prefijo} {this.Nombre} {this.Apellidos}");
+                    Console.WriteLine($"{prefijo} {this.nombre} {this.Apellidos}");
                 }
-
 
             }
             else
             {
-                Console.WriteLine($"{this.Nombre}, {this.Apellidos} ");
+                Console.WriteLine($"{this.nombre}, {this.Apellidos} ");
             }
 
         }
 
         public string NombreCompleto()
         {
-            return $"{this.Nombre} {this.Apellidos}";
+            return $"{this.nombre} {this.Apellidos}";
         }
+
+        public Alumno GetObject()
+        {
+            return this;
+        }
+    }
+
+    public struct Alumno2
+    {
+        public string Nombre;
+        public string Apellidos;
+        public int Edad;
+
     }
 
     public class Calculos
@@ -104,6 +246,30 @@ namespace Formacion.CSharp.Objects
             resultado = a + b;
             Console.WriteLine("Método suma: el resultado es {0}", resultado);
 
+        }
+
+        public void TomarDatos(Alumno a)
+        {
+            Console.Write("Nombre: ");
+            a.Nombre = Console.ReadLine();
+            Console.Write("Apellidos: ");
+            a.Apellidos = Console.ReadLine();
+        }
+
+        public void TomarDatos2(Alumno2 a)
+        {
+            Console.Write("Nombre: ");
+            a.Nombre = Console.ReadLine();
+            Console.Write("Apellidos: ");
+            a.Apellidos = Console.ReadLine();
+        }
+
+        public void TomarDatos2Ref(ref Alumno2 a)
+        {
+            Console.Write("Nombre: ");
+            a.Nombre = Console.ReadLine();
+            Console.Write("Apellidos: ");
+            a.Apellidos = Console.ReadLine();
         }
     }
 }
---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index(int id)
+        {
+
+            // Pasar datos a la vida con ViewBag
+            ViewBag.numero = id;
+            ViewBag.mensaje = $"Tabla de multiplicar del {id}";
+
+            //Pasar datos a la vista como modelo de datos
+            //return View(id);
+
+            return View(id);
+        }
+
+        public IActionResult Demo()
+        {
+            return View();
+        }
+    }
+}
