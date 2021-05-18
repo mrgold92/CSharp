333e406d8d6acccc6c3e5dc9643796ae45b5e71b
David Salazar <davids1_4@hotmail.com>
2021-05-18 19:41:55 +0200

    Añadidos ejercicios A y B

---
+++ AA-Ejercicios/A/A.cs
@@ -0,0 +1,52 @@
+﻿/*************************************************
+* A1. Pregunta el nombre de Operador
+* y muestra un saludo incluyendo el contenido
+* de la variable cadena.
+* 
+* A2. Muestra el saludo en mayúsculas, minúsculas
+* y con la letra de cada palabra en mayúsculas.
+**************************************************/
+
+using System;
+
+namespace Ejercicios
+{
+    class A
+    {
+        static void Main(string[] args)
+        {
+
+            string cadena = "Hola mundo !!";
+            Console.Write("Dime tu nombre: ");
+            string nombre = Console.ReadLine();
+            string saludo = $"{cadena}, ¿Qué tal {nombre}?";
+
+            //A1
+            Console.WriteLine(saludo);
+
+            //A2
+            Console.WriteLine(saludo.ToLower());
+            Console.WriteLine(saludo.ToUpper());
+            Console.WriteLine(ToTitle(saludo));
+
+        }
+        /// <summary>
+        /// Capitaliza cada palabra de una frase
+        /// </summary>
+        /// <param name="frase"></param>
+        /// <returns></returns>
+        static string ToTitle(string frase)
+        {
+            string fraseTitle = "";
+            string[] fraseSeparada = frase.Split(" ");
+
+            for (int i = 0; i < fraseSeparada.Length; i++)
+            {
+                fraseTitle += fraseSeparada[i].Substring(0, 1).ToUpper() + fraseSeparada[i].Substring(1);
+                if (fraseSeparada.Length - 1 != i) fraseTitle += " ";
+            }
+
+            return fraseTitle;
+        }
+    }
+}
---
+++ AA-Ejercicios/B/B.cs
@@ -0,0 +1,32 @@
+﻿using System;
+/****************************************************************
+ * B1.Pregunta un número al operador y muestra el resultado de 
+ * multiplicarlo por PI. Utiliza la constante "math.pi" y 
+ * recuerda incluir "import math".
+ * 
+ * B2. Muestra la raíz cuadrada del mismo número.
+ * B3. Muestra el arco coseno del mismo número.
+ * **************************************************************/
+
+namespace Ejercicios
+{
+    class B
+    {
+        static void Main(string[] args)
+        {
+            //B1
+            Console.Write("Dime un número: ");
+            int.TryParse(Console.ReadLine(), out int numero);
+            Console.WriteLine($"{numero} * PI = {(numero * Math.PI).ToString("#.##")}");
+
+            //B2
+            Console.WriteLine($"Raíz cuadrada de {numero}: {Math.Sqrt(numero)}");
+
+            //B3
+            if (numero >= -1 && numero <= 1)
+            {
+                Console.WriteLine($"Arco coseno de {numero}: {Math.Acos(numero)}");
+            }
+        }
+    }
+}
