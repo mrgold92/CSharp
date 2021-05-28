479d8fb238144950f5c2d5387858e691a70120c9
David Salazar <davids1_4@hotmail.com>
2021-05-28 14:02:36 +0200

    Clase 28/05/2021

---
+++ Herencia/Program.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Herencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //DEMOB hereda de DEMOA
+            var demo = new DemoB();
+
+            demo.Nombre = "David";
+            demo.Apellidos = "Salazar";
+            demo.Edad = 28;
+
+            demo.PintarDatos(); //hereda de A <-- Está sobreescrito en B
+            demo.PintarDatos2(); //método propio de B
+            demo.PintarDatos3(); //método propio de B que llama a pintardatos de A (padre)
+
+            Console.WriteLine("------------------------");
+
+            //Interfaces
+            IElectrodomestico electrodomestico;
+            IDispositivosDomotica domotica;
+
+            Lavadora lavadora1 = new Lavadora() { Nombre = "Lavadora 1", Revoluciones = 750, Color = "Blanco" };
+            IElectrodomestico lavadora2 = new Lavadora() { Nombre = "Lavadora 2", Revoluciones = 1000, Color = "Gris" };
+            Nevera nevera1 = new Nevera() { Nombre = "Nevera 1", ConsumoWatios = 250, Color = "Verde" };
+
+
+            Console.WriteLine(lavadora1.GetType().ToString());
+            lavadora1.Encender();
+            lavadora1.Apagar();
+            lavadora1.PintarFicha();
+
+            Console.WriteLine();
+
+            Console.WriteLine(lavadora2.GetType().ToString());
+            lavadora2.Encender();
+            lavadora2.Apagar();
+            //lavadora2.PintarFicha(); // error
+            ((Lavadora)lavadora2).PintarFicha();
+
+            Console.WriteLine("-------------------");
+
+            nevera1.Encender();
+            nevera1.Apagar();
+
+            domotica = nevera1;
+            domotica.Encender();
+            domotica.Apagar();
+
+            electrodomestico = nevera1;
+
+            electrodomestico.Encender();
+            electrodomestico.Apagar();
+
+
+
+        }
+    }
+
+
+    class DemoA
+    {
+
+        public string Nombre { get; set; }
+
+        public string Apellidos { get; set; }
+
+        public int Edad { get; set; }
+
+        public virtual void PintarDatos()
+        {
+            Console.WriteLine($"{Nombre} {Apellidos}");
+        }
+
+    }
+
+    //Con sealed sellamos la clase, es decir, impedimos que nadie puede heredar
+    class DemoB : DemoA
+    {
+        //También podemos sellar métodos, para que no podamos sobreescribirlo en otro lado
+        sealed public override void PintarDatos()
+        {
+            Console.WriteLine($"Nombre: {Nombre}");
+            Console.WriteLine($"Apellidos: {Apellidos}");
+        }
+
+        public void PintarDatos2()
+        {
+            Console.WriteLine($"{Nombre} {Apellidos} {Edad}");
+        }
+
+        public void PintarDatos3()
+        {
+            base.PintarDatos(); //llamada a padre
+            Console.WriteLine($"{Edad}");
+        }
+
+
+    }
+
+    //Demob está sellada y no podemos heredarla.
+    //class DemoC : DemoB { }
+
+    class DemoC
+    {
+        //IQueryable = consultable, solo trae los datos cuando se quiere acceder a la información
+        //ej: al recorrer una lista que previamente hemos cargado con una consulta linq
+
+    }
+
+    //INTERFACES
+    interface IElectrodomestico
+    {
+        int ConsumoWatios { get; set; }
+        string Nombre { get; set; }
+        string Color { get; set; }
+
+        void Encender();
+        void Apagar();
+    }
+
+    interface IDispositivosDomotica
+    {
+        public void Apagar();
+
+        public void Encender();
+    }
+
+    class Nevera : IElectrodomestico, IDispositivosDomotica
+    {
+        public int ConsumoWatios { get; set; }
+        public string Nombre { get; set; }
+        public string Color { get; set; }
+        public int Puertas { get; set; }
+        public int TempMin { get; set; }
+
+        public void Encender()
+        {
+            Console.WriteLine("Nevera On");
+        }
+
+        public void Apagar()
+        {
+            Console.WriteLine("Nevera Off");
+        }
+
+        void IDispositivosDomotica.Encender()
+        {
+            Console.WriteLine("Domótica Nevera On");
+        }
+
+        void IDispositivosDomotica.Apagar()
+        {
+            Console.WriteLine("Domótica Nevera Off");
+        }
+
+        void IElectrodomestico.Encender()
+        {
+            Console.WriteLine("Electro Nevera On");
+        }
+
+        void IElectrodomestico.Apagar()
+        {
+            Console.WriteLine("Electro Nevera Off");
+        }
+
+        public void TurboCool()
+        {
+            Console.WriteLine("Turbo On");
+        }
+    }
+
+    class Lavadora : IElectrodomestico
+    {
+        public int ConsumoWatios { get; set; }
+        public string Nombre { get; set; }
+        public string Color { get; set; }
+        public int Revoluciones { get; set; }
+
+
+        public void Apagar()
+        {
+            Console.WriteLine("Nevera Off");
+        }
+
+        public void Encender()
+        {
+            Console.WriteLine("Nevera On");
+        }
+
+        public void PintarFicha()
+        {
+            Console.WriteLine($"Lavadora de color {Color}, con un máximo de {Revoluciones} revoluciones.");
+        }
+    }
+}
---
+++ Tasks/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Tasks
+{
+    class Program
+    {
+        delegate void Demo1();
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Inicio de la app");
+            CreacionTareas();
+            Console.WriteLine("Fin de la app");
+
+            Console.ReadKey();
+        }
+
+
+        static async void CreacionTareas()
+        {
+            //un delegado es un tipo de dato que contiene código
+            //Action es un tipo de delegado que existe en .NET que nos sirve para especificar el código de un método
+
+            Demo1 demo = Saludo;
+
+            //Demo1 demo = Saludo2; //error porque saludo2 tiene parámetro
+
+            //con action
+            Task tarea1 = new Task(new Action(Saludo));
+
+            //con función lambda
+            Task<bool> tarea2 = new Task<bool>(() =>
+            {
+                Console.WriteLine("Tarea 2 ejecutándose");
+                Thread.Sleep(5000);
+                return true;
+            });
+
+            //con delegate
+            Task tarea3 = new Task(delegate
+            {
+                Console.WriteLine("Tarea 3 ejecutándose");
+            });
+
+
+            Task tarea4 = new Task(new Action(demo));
+
+            Task tarea5 = Task.Run(() =>
+            {
+                Console.WriteLine("Tarea 5 ejecutándose");
+                Thread.Sleep(5000);
+                Console.WriteLine("Tarea 5 fin");
+            });
+
+            tarea5.Wait(1000); //Esperamos a que finalice la 5
+
+            tarea1.Start();
+
+            tarea2.Start();
+            //tarea2.Wait(1000); //Esperamos a que finalice la 2
+
+            // Con el result esperamos automáticamente a que finalice para pintar el resultado.
+            // El result también bloquea el hilo principal, para arreglarlo,
+            // tenemos que hacer que los métodos sean asíncronos.
+            //Console.WriteLine($"Resultado tarea 2: {tarea2.Result}");
+            Console.WriteLine($"Resultado tarea 2: {await tarea2}");
+
+            tarea3.Start();
+            tarea4.Start();
+
+        }
+        static void Procesos()
+        {
+            Saludo();
+            Console.WriteLine("Tarea 2 ejecutándose");
+            Console.WriteLine("Tarea 3 ejecutándose");
+            Saludo();
+            Console.WriteLine("Tarea 5 ejecutándose");
+
+        }
+        static void Saludo()
+        {
+            Console.WriteLine("Hola mundo !!!");
+        }
+        static void Saludo2(string nombre)
+        {
+            Console.WriteLine($"Hola {nombre} !!!");
+        }
+        static void Saludo3()
+        {
+            Console.WriteLine("Hola a todos !!!");
+        }
+    }
+
+
+}
